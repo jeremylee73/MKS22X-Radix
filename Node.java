0b860678d644c81aaa3014135fab37d05011cc56
Jeremy Lee <jlee00@stuy.edu>
2019-03-29 11:47:04 -0400

    Added Node

--- Node.java
+++ Node.java
@@ -1,29 +1,29 @@
-class Node{
-  private int data;
-  private Node prev, next;
+public class Node<E>{
+  private E data;
+  private Node<E> prev, next;
 
-  public Node(int data, Node prev, Node next){
+  public Node(E data, Node<E> prev, Node<E> next){
     this.data = data;
     this.prev = prev;
     this.next = next;
   }
 
-  public int getData(){
+  public E getData(){
     return data;
   }
-  public Node getNext(){
+  public Node<E> getNext(){
     return next;
   }
-  public Node getPrev(){
+  public Node<E> getPrev(){
     return prev;
   }
-  public void setData(int data){
+  public void setData(E data){
     this.data = data;
   }
-  public void setNext(Node next){
+  public void setNext(Node<E> next){
     this.next = next;
   }
-  public void setPrev(Node prev){
+  public void setPrev(Node<E> prev){
     this.prev = prev;
   }
 }
