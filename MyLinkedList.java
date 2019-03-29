bdbbb2a563a4d60cfbc833db7f495cc59cf25d04
Jeremy Lee <jlee00@stuy.edu>
2019-03-29 11:40:26 -0400

    Added template

---
+++ MyLinkedList.java
@@ -0,0 +1,29 @@
+public class MyLinkedList<E>{
+  private Node start;
+  private Node end;
+  private int size;
+
+  public MyLinkedList(){
+
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public void clear(){
+
+  }
+
+  public boolean add(E e){
+    return true;
+  }
+
+  public void extend(MyLinkedList<E> other){
+
+  }
+
+  public E removeFront(){
+    return start;
+  }
+}
