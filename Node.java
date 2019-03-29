e53f3a052dba023480d5da8a37f32940ac176f76
Jeremy Lee <jlee00@stuy.edu>
2019-03-29 11:41:56 -0400

    Imported Node from past lab

---
+++ Node.java
@@ -0,0 +1,29 @@
+class Node{
+  private int data;
+  private Node prev, next;
+
+  public Node(int data, Node prev, Node next){
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
+  }
+
+  public int getData(){
+    return data;
+  }
+  public Node getNext(){
+    return next;
+  }
+  public Node getPrev(){
+    return prev;
+  }
+  public void setData(int data){
+    this.data = data;
+  }
+  public void setNext(Node next){
+    this.next = next;
+  }
+  public void setPrev(Node prev){
+    this.prev = prev;
+  }
+}
