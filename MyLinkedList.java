4d2e3c1e799d944aaf6b4394ef4cd88e6bf3e155
Jeremy Lee <jlee00@stuy.edu>
2019-03-30 21:14:06 -0400

    Added extend

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,10 +1,10 @@
 public class MyLinkedList<E>{
-  private Node start;
-  private Node end;
+  private Node<E> start;
+  private Node<E> end;
   private int size;
 
   public MyLinkedList(){
-
+    size = 0;
   }
 
   public String toString(){
@@ -19,11 +19,25 @@ public class MyLinkedList<E>{
     return true;
   }
 
-  public void extend(MyLinkedList<E> other){
+  public int size(){
+    return size;
+  }
 
+  public void extend(MyLinkedList<E> other){
+    if (size == 0) {
+      start = other.start;
+      end = other.end;
+    } else if (other.size() != 0) {
+      end.setNext(other.start);
+      other.start.setPrev(end);
+    }
+    other.start = null;
+    other.end = null;
+    size += other.size();
+    other.size = 0;
   }
 
   public E removeFront(){
-    return start;
+    return start.getData();
   }
 }
