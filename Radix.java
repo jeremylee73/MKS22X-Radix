9791a919dc1d38e9402f59d0139078694eba8b02
Jeremy Lee <jlee00@stuy.edu>
2019-04-02 14:48:59 -0400

    Fixing bucket assignment

--- Radix.java
+++ Radix.java
@@ -1,18 +1,28 @@
 public class Radix{
-  public static MyLinkedList<Integer>[] buckets;
+  private static MyLinkedList<Integer>[] buckets;
 
   public static void radixsort(int[] data){
     buckets = new MyLinkedList[20];
-    for (int i=0; i<data.length; i++){
-      if (data[i] % 10 == i){
+    int maxD = maxDigit(data);
+//    for (int d=1; d<=maxD; d++){
+      for (int i=0; i<data.length; i++){
+        int lastDigit = data[i] % 10;
         if (data[i] < 0){
-          buckets[9-i].add(data[i]);
+          buckets[9-lastDigit].add(data[i]);
         } else {
-          buckets[10+i].add(data[i]);
+          buckets[10+lastDigit].add(data[i]);
         }
       }
-    }
 
+      int count = 0;
+      for (int i=0; i<buckets.length; i++){
+        System.out.println(buckets[i]);
+        // while (buckets[i].size() > 0){
+        //   data[count] = buckets[i].removeFront();
+        //   count++;
+        // }
+      }
+    //}
   }
 
   private static int maxDigit(int[] data){
@@ -34,6 +44,11 @@ public class Radix{
 
   public static void main(String[] args){
     int[] test = {1,2,3, -300000};
-    System.out.println(maxDigit(test));
+    radixsort(test);
+    System.out.print("[");
+    for (int i=0; i<test.length; i++){
+      System.out.print(test[i] + ",");
+    }
+    System.out.println("]");
   }
 }
