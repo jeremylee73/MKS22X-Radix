276aac4faac7608802c8eb937db656c82d29be79
Jeremy Lee <jlee00@stuy.edu>
2019-04-02 14:15:47 -0400

    Added buckets

--- Radix.java
+++ Radix.java
@@ -1,5 +1,14 @@
 public class Radix{
   public static void radixsort(int[] data){
-    
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    for (int i=0; i<data.length; i++){
+      if (data[i] % 10 == i){
+        if (data[i] < 0){
+          buckets[9-i].add(data[i]);
+        } else {
+          buckets[10+i].add(data[i]);
+        }
+      }
+    }
   }
 }
