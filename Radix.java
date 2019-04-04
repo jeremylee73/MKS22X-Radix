db67097d57ce2f65210d4e093c6ad13c3d203b6f
Jeremy Lee <jlee00@stuy.edu>
2019-04-03 22:57:18 -0400

    Added bucket extensions for d!=1

--- Radix.java
+++ Radix.java
@@ -15,36 +15,28 @@ public class Radix{
           int pos = (int) (n / Math.pow(10,i-1)) % 10;
           buckets[pos+9].add(n);
         }
-        
         temp.clear();
-
         for (int i = 0; i < 20; i++) {
           temp.extend(buckets[i]);
         }
-
         for (MyLinkedList<Integer> bucket : buckets) {
           bucket.clear();
         }
-
+      } else {
+        for (int i=0; i<data.length; i++) {
+          int n = temp.removeFront();
+          int pos = (int)(n / (Math.pow(10,i-1))) % 10;
+          buckets[pos+9].add(n);
+        }
+        temp.clear();
+        for (int i=0; i<20; i++) {
+          temp.extend(buckets[i]);
+        }
+        for (MyLinkedList<Integer> bucket : buckets) {
+          bucket.clear();
+        }
       }
     }
-    // for (int i=0; i<data.length; i++){
-    //   int lastDigit = data[i] % 10;
-    //   if (data[i] < 0){
-    //     buckets[9-lastDigit].add(data[i]);
-    //   } else {
-    //     buckets[10+lastDigit].add(data[i]);
-    //   }
-    // }
-    //
-    //   int count = 0;
-    //   for (int i=0; i<buckets.length; i++){
-    //     System.out.println(buckets[i]);
-    //     // while (buckets[i].size() > 0){
-    //     //   data[count] = buckets[i].removeFront();
-    //     //   count++;
-    //     // }
-    //   }
   }
 
   private static int maxDigit(int[] data){
