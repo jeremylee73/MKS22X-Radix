7e7aa78d5024e416a96d0a3a3735fd673d271c16
Jeremy Lee <jlee00@stuy.edu>
2019-04-08 14:40:12 -0400

    working extend

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,10 +1,12 @@
 public class MyLinkedList<E>{
-  private Node<E> start;
-  private Node<E> end;
+  private Node start;
+  private Node end;
   private int size;
 
   public MyLinkedList(){
     size = 0;
+    start = null;
+    end = null;
   }
 
   public String toString(){
@@ -12,7 +14,7 @@ public class MyLinkedList<E>{
     if (size == 0 || start == null || end == null){
       return "[]";
     }
-    Node<E> current = start;
+    Node current = start;
     while (current.getNext() != null){
       ans += current.getData() + ",";
       current = current.getNext();
@@ -27,12 +29,12 @@ public class MyLinkedList<E>{
     end = null;
   }
 
-  public boolean add(E e){
-    if (end == null) { // if list is totally empty
-      end = new Node<E>(e,end,null);
+  public boolean add(int e){
+    if (end == null) {
+      end = new Node(e,end,null);
       start = end;
     } else {
-      Node<E> newNode = new Node<E>(e, end.getPrev(), null);
+      Node newNode = new Node(e, end.getPrev(), null);
       end.setNext(newNode);
       end = newNode;
     }
@@ -50,27 +52,15 @@ public class MyLinkedList<E>{
       end = other.end;
     } else if (other.size() != 0) {
       end.setNext(other.start);
+      end = other.end;
       other.start.setPrev(end);
     }
-    other.start = null;
-    other.end = null;
     size += other.size();
-    other.size = 0;
   }
 
-  private Node<E> getNode(int index) {
-    int current = 0;
-    Node<E> ans = start;
-    while (current < index) {
-      ans = ans.getNext();
-      current++;
-    }
-    return ans;
-  }
-
-  public E remove(int index){
-    E ans = getNode(index).getData();
-    start = getNode(index+1);
+  public int removeFront(){
+    int ans = start.getData();
+    start = start.getNext();
     return ans;
   }
 
@@ -81,9 +71,9 @@ public class MyLinkedList<E>{
     test.add(2);
     test.add(3);
     System.out.println(test);
-    System.out.println(test.remove(0));
-    System.out.println(test.remove(0));
-    System.out.println(test.remove(0));
+    System.out.println(test.removeFront());
+    System.out.println(test.removeFront());
+    System.out.println(test.removeFront());
     System.out.println(test);
   }
 }
