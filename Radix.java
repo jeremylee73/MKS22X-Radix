9b21842501f2dde3e35582000b9a298f7774841f
Jeremy Lee <jlee00@stuy.edu>
2019-04-08 14:46:18 -0400

    Fixed Radix

--- Radix.java
+++ Radix.java
@@ -1,5 +1,6 @@
-public class Radix{
+import java.util.*;
 
+public class Radix{
   public static void radixsort(int[] data){
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     MyLinkedList<Integer> temp = new MyLinkedList<Integer>();
@@ -12,10 +13,9 @@ public class Radix{
       if (d == 1){
         for (int i=0; i<data.length; i++){
           int n = data[i];
-          int pos = (int) (n / Math.pow(10,i-1)) % 10;
+          int pos = (int) (n / Math.pow(10,d-1)) % 10;
           buckets[pos+9].add(n);
         }
-        temp.clear();
         for (int i = 0; i < 20; i++) {
           temp.extend(buckets[i]);
         }
@@ -25,7 +25,7 @@ public class Radix{
       } else {
         for (int i=0; i<data.length; i++) {
           int n = temp.removeFront();
-          int pos = (int)(n / (Math.pow(10,i-1))) % 10;
+          int pos = (int)(n / (Math.pow(10,d-1))) % 10;
           buckets[pos+9].add(n);
         }
         temp.clear();
@@ -59,13 +59,38 @@ public class Radix{
     return maxString.length();
   }
 
-  public static void main(String[] args){
-    int[] test = {1,2,3, -300000};
-    radixsort(test);
-    System.out.print("[");
-    for (int i=0; i<test.length; i++){
-      System.out.print(test[i] + ",");
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          radixsort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
     }
-    System.out.println("]");
   }
 }
